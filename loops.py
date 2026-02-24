a23db2229a25d6dbe1a1982c37c893cbdfc4f15b
Walter Mokwena <mokwenawalter50@gmail.com>
2026-02-24 09:49:38 +0000

    learnt loops

---
+++ loops.py
@@ -0,0 +1,14 @@
+1#know how to write a for loop as a while loop
+#range: allows you to go forward and back wards
+start = 0
+stop = 10
+count = 1
+
+#for i in range(start, stop, count):
+ #   print(f"{i}")
+i = start
+while start <= i < stop:
+    print(i,".\t", i*i)
+    i += 1
+
+
