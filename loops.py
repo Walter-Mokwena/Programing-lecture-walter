c64b8a2509dee208e053e9e6fb14ccff74b5ccc5
Walter Mokwena <mokwenawalter50@gmail.com>
2026-03-10 20:09:28 +0000

    Rewrote question 2 from the test

--- loops.py
+++ loops.py
@@ -1,5 +1,5 @@
 string = input()
-
+flag = 0
 char = "-_ "
 new_string = ""
 new_string += string[0].lower()
@@ -15,5 +15,5 @@ for i in range(1,len(string)):
             flag = False
             continue
         new_string += string[i]
-        
+
 print(new_string)
